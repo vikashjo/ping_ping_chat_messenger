cfbaf3b99f0467daeaa0f64e4fb93020ebd29ce9
vikashjo <vikashjoshi431@gmail.com>
2024-05-08 17:57:19 +0530

    Enable flash messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,4 +20,7 @@ require("channels")
 
 $(document).on('turbolinks:load', function() {
     $('.ui.dropdown').dropdown();
+    $('.message .close').on('click', function() {
+      $(this).closest('.message').transition('fade');
+  });
 })
