d183a1ee302960e436eff893d448e5d20027665a
vikashjo <vikashjoshi431@gmail.com>
2024-05-17 17:14:19 +0530

    Display message realtime

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,10 @@
 class MessagesController < ApplicationController
-    before_action :require_user
+    # before_action :require_user
     def create
         message = current_user.messages.build(message_params)
         if message.save
-            redirect_to root_path
+            ActionCable.server.broadcast "chatroom_channel",
+                                          mod_message: message_render(message)
         end
     end
 
@@ -11,5 +12,9 @@ class MessagesController < ApplicationController
 
     def message_params
         params.require(:message).permit(:body)
+    end
+
+    def message_render
+        render(partial: 'message', locals: {message: message})
     end
 end
