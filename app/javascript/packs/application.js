5f49d8da932a4d7a9724533789b98e1b842c1403
vikashjo <vikashjoshi431@gmail.com>
2024-05-19 12:36:49 +0530

    Modify display

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,6 +18,12 @@ require("channels")
 //= require jquery
 //= require semantic-ui
 
+scroll_bottom = function() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
 $(document).on('turbolinks:load', function() {
     $('.ui.dropdown').dropdown();
     $('.message .close').on('click', function() {
